f567a9b0ee5c1aa38630d4eeb61cc60f4386c0c2
ismail <ismailjosim99@gmail.com>
2023-08-28 02:49:23 +0600

    Add new line, tab & single back \

--- m-01-basic-syntax-var-data-types/hello.c
+++ m-01-basic-syntax-var-data-types/hello.c
@@ -2,6 +2,12 @@
 
 int main()
 {
-    printf("Hello World");
+    printf("Welcome to C Programming.\n My journey Start Here!");
     return 0;
 }
+
+/*
+* \n => new line
+* \t => add tab space
+* \\ => print single back \
+*/
