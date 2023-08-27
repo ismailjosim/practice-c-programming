cf848a208f867ef795256e38f1e67ec213ea953d
ismail <ismailjosim99@gmail.com>
2023-08-28 03:34:55 +0600

    How to Take Input in C

---
+++ m-01-basic-syntax-var-data-types/input.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+// get value from user
+
+int main()
+{
+    // int rahim;
+    // scanf("%d", &rahim);
+    // printf("%d", rahim);
+
+    // get % from user
+     int a, b;
+    scanf("%d%% %d%%", &a, &b);
+    printf("%d%% %d%%\n", a, b); // Added \n to print a newline
+
+    return 0;
+}
+
+/*
+scanf("%d", &name)
+
+
+*/
