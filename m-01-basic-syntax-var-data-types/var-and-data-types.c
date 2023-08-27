ee31bf3a8aa4b77d1ba4082f2f46922cd4e5f7d4
ismail <ismailjosim99@gmail.com>
2023-08-28 03:11:23 +0600

    variables and data types

---
+++ m-01-basic-syntax-var-data-types/var-and-data-types.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//int name = 10; // int type data take 4 bytes store from (RAM) memory. Here equal(=) sign means assignment operator in Programming and 10 is the value of name.
+
+
+int main()
+{
+    int integerValue = 10;
+    float floatValue = 10.52;
+    char name = 'I';
+    printf("%0.2f",floatValue); // print 2 digit in float number.%0.2f % then o.2 means how many digit we want to get and f means float.
+    return 0;
+}
